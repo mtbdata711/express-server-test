daf23737b05c693f6061215ff0ce72a04b3f93ae
Matthew <bengethomasmatthew@gmail.com>
2020-10-08 17:23:12 +0100

    Cors && re-shape data

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
-const HTTP_PORT = 8000;
+const cors = require('cors')
+const HTTP_PORT = 8080;
 const DBSOURCE = "instagram.db";
+
+app.use(cors())
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if(err){
@@ -21,6 +24,19 @@ app.get("/all", (req, res, next) => {
           res.status(400).json({"error":err.message});
           return;
         }
+
+        rows = rows.map( row => {
+            return {
+                id: row.id,
+                college: row.college,
+                created_at: row.created_at,
+                last_updated: row.last_updated,
+                last_updated_token: row.last_updated_token,
+                status: row.cron_status,
+                last_error: row.last_error,
+                data: JSON.parse(row.cache)
+            }
+        });
         res.json({
             "message":"success",
             "data":rows
